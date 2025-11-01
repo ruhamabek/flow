36971fd6da537819d5a03ef9d704740f6d4d2b46
ruhamabek <ruhamauzubo@gmail.com>
2025-11-01 12:11:38 +0300

    fix: replacing it with chrome-aws-lambda

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -1,45 +1,31 @@
-// executor for launching browser: LaunchBrowserExecutor.ts
-import { ExecutionEnviroment } from "@/app/types/executor";
+ import { ExecutionEnviroment } from "@/app/types/executor";
 import { LaunchBrowserTask } from "../task/LaunchBrowser";
-import chromium from "@sparticuz/chromium";
+import chromium from "chrome-aws-lambda";
 import puppeteer from "puppeteer-core";
 
 export async function LaunchBrowserExecutor(
-    enviroment: ExecutionEnviroment<typeof LaunchBrowserTask>,
+  enviroment: ExecutionEnviroment<typeof LaunchBrowserTask>
 ): Promise<boolean> {
-    const viewport = {
-  deviceScaleFactor: 1,
-  hasTouch: false,
-  height: 1080,
-  isLandscape: true,
-  isMobile: false,
-  width: 1920,
-}
+  try {
+    const websiteUrl = enviroment.getInput("Website Url");
 
-    try {
-        const websiteUrl = enviroment.getInput("Website Url");
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: true,
+    });
 
-         const browser = await puppeteer.launch({
-            headless: true,
-            args: chromium.args,
-            defaultViewport: viewport,
-            executablePath: await chromium.executablePath(),
-        });
+    enviroment.log.info("Browser launched successfully");
+    enviroment.setBrowser(browser);
 
-        enviroment.log.info("Browser launched successfully");
-        enviroment.setBrowser(browser);
-
-        const page = await browser.newPage();
-        await page.goto(websiteUrl);
-        enviroment.setPage(page);
-        enviroment.log.info(`Opened page at: ${websiteUrl}`);
-        return true;
-    } catch (error) {
-        if (error instanceof Error) {
-            enviroment.log.error(error.message);
-        } else {
-            enviroment.log.error(String(error));
-        }
-        return false;
-    }
+    const page = await browser.newPage();
+    await page.goto(websiteUrl);
+    enviroment.setPage(page);
+    enviroment.log.info(`Opened page at: ${websiteUrl}`);
+    return true;
+  } catch (error) {
+    enviroment.log.error(error instanceof Error ? error.message : String(error));
+    return false;
+  }
 }

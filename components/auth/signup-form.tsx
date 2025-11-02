4bcf2e8531b56589ccc5790ac8a35c1b818301eb
ruhamabek <ruhamauzubo@gmail.com>
2025-11-02 16:24:41 +0300

    edited return url

--- components/auth/signup-form.tsx
+++ components/auth/signup-form.tsx
@@ -235,12 +235,13 @@ export function SignupForm({
                     },
                     onSuccess: ( ) => {
                       toast.success("Authentication Redirecting...");
+                      router.push("/setup");
                     },
                     onError: (ctx) => {
                       setError(ctx.error.message);
                     },
                   },
-                     callbackURL: "https://flow-chi-drab.vercel.app/setup"
+                      
                 });
               }}
               className="group w-full cursor-pointer  space-x-1 py-3 flex transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f_inset]  border-white/10  items-center justify-center border rounded-lg hover:bg-transparent/20 duration-150 active:bg-transparent/50"
